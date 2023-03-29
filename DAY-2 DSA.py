13bdf907df717668e14da58e1968179d546f7733
Ayusman@8080 <112675549+ayusman1234@users.noreply.github.com>
2023-03-29 19:03:27 +0530

    Add files via upload

---
+++ DAY-2 DSA.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+list.headval.nextval = e2
+e2.nextval = e3
+list.listprint()
+
+
+# In[3]:
+
+
+#insertion at beginning
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+# Print the linked list
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+   def AtBegining(self,newdata):
+      NewNode = Node(newdata)
+      # Update the new nodes next val to existing node
+      NewNode.nextval = self.headval
+      self.headval = NewNode
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+list.headval.nextval = e2
+e2.nextval = e3
+list.AtBegining("Sun")
+list.listprint()
+
+
+# In[4]:
+
+
+#insertion at the end
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+# Function to add newnode
+   def AtEnd(self, newdata):
+      NewNode = Node(newdata)
+      if self.headval is None:
+         self.headval = NewNode
+         return
+      laste = self.headval
+      while(laste.nextval):
+         laste = laste.nextval
+      laste.nextval=NewNode
+# Print the linked list
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+
+list.headval.nextval = e2
+e2.nextval = e3
+list.AtEnd("Thu")
+list.listprint()
+
+
+# In[5]:
+
+
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+   def Inbetween(self,middle_node,newdata):
+      if middle_node is None:
+         print("The mentioned node is absent")
+         return
+      NewNode = Node(newdata)
+      NewNode.nextval = middle_node.nextval
+      middle_node.nextval = NewNode
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+list = SLinkedList()
+list.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Thu")
+list.headval.nextval = e2
+e2.nextval = e3
+list.Inbetween(list.headval.nextval,"Fri")
+list.listprint()
+
+
+# In[ ]:
+
+
+
+
